0ac8035f66d0a1928395b3b9e7894b0389dec03b
ashjw <ashjw592@gmail.com>
2026-05-19 11:39:56 -0600

    added an argument for changing which ollama model to use

--- query.py
+++ query.py
@@ -22,12 +22,16 @@ Answer the question based on the above context: {question}
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("query_text", type=str, help="The query text.")
+    parser.add_argument(
+        "--model", type=str, default="mistral", help="The Ollama model to use."
+    )
     args = parser.parse_args()
     query_text = args.query_text
-    query_rag(query_text)
+    model_name = args.model
+    query_rag(query_text, model_name)
 
 
-def query_rag(query_text: str):
+def query_rag(query_text: str, model_name: str = "mistral") -> str:
     embedding_function = get_embedding_function()
     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
 
@@ -37,7 +41,7 @@ def query_rag(query_text: str):
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     prompt = prompt_template.format(context=context_text, question=query_text)
 
-    model = OllamaLLM(model="mistral")
+    model = OllamaLLM(model=model_name)
     response_text = model.invoke(prompt)
 
     sources = [doc.metadata.get("id", None) for doc, _score in results]
